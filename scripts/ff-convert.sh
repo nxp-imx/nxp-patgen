31f798e215d0c0d91bd9e2f604a60d38a14e6d99
Oliver Brown <oliver.brown@nxp.com>
2021-10-11 08:49:25 -0500

    Split the bitmap conversion out of the test.sh script.
    This can now be run as a separate make target.
    
    Signed-off-by: Oliver Brown <oliver.brown@nxp.com>

--- scripts/ff-convert.sh
+++ scripts/ff-convert.sh
@@ -4,15 +4,18 @@ outextension=$2
 
 outfilebase=`echo $1 | awk -F'-' '{print $1 "-" $2}'`
 resolution=`echo $1 | awk -F'-' '{print $3}'`
-informat=`echo $1 | awk -F'-' '{print $4}' | awk -F. '{print $1}'`
+informat=`echo $1 | awk -F'-' '{print $4}' | awk -F. '{print $1}' | awk -F_ '{print $1}'`
+informat_str=`echo $1 | awk -F'-' '{print $4}' | awk -F. '{print $1}'`
 
-#debug=`echo $1 | awk -F'-' '{print " 1- " $1 " 2- " $2  " 3- " $3 " 4- "  $4}'`
-#echo "debug = $debug"
+debug=`echo $1 | awk -F'-' '{print " 1- " $1 " 2- " $2  " 3- " $3 " 4- "  $4}'`
+echo "debug = $debug"
 
-outfile=$outfilebase-$resolution-$informat-$outformat.$outextension
+outfile=${outfilebase}-${resolution}-${informat_str}-${outformat}.${outextension}
 
-echo converting $1 from $informat to $outformat
-echo resolution is $resolution
+echo converting ${1} from ${informat} to ${outformat}
+#echo resolution is ${resolution}
+#echo "informat: ${informat}"
+#echo "outfile: ${outfile}"
 
-ffmpeg -pix_fmt $informat -video_size $resolution -i $1  $outfile
+ffmpeg -pix_fmt ${informat} -video_size ${resolution} -i ${1}  ${outfile}
 
---
+++ scripts/run-ff-convert.sh
@@ -0,0 +1,24 @@
+#/bin/sh
+DIR=$1
+CONTAINER_FMT=bmp
+
+echo "-----------------------------------------------------------"
+for file in $DIR/*.rgb ; do
+     [ -f "$file" ] || continue
+     echo "-----------------------------------------------------------"
+     echo converting $file to $CONTAINER_FMT
+     ../scripts/ff-convert.sh $file $CONTAINER_FMT &
+done
+wait 
+
+echo "-----------------------------------------------------------"
+for file in $DIR/*.yuv ; do
+     [ -f "$file" ] || continue
+     echo "-----------------------------------------------------------"
+     echo converting $file to $CONTAINER_FMT
+     ../scripts/ff-convert.sh $file $CONTAINER_FMT &
+done
+echo "-----------------------------------------------------------"
+wait
+
+
