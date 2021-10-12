e24fe958af238f6e39f699857c6c2fe29db9992f
Oliver Brown <oliver.brown@nxp.com>
2021-10-12 08:59:08 -0500

    Added 16 million color test.
    Signed-off-by: Oliver Brown <oliver.brown@nxp.com>

--- scripts/test-quality.sh
+++ scripts/test-quality.sh
@@ -39,7 +39,7 @@ time ./patgen.exe -v -pattern hsv       -vsize $RES -pix_fmt $FMT -outname "$DIR
 time ./patgen.exe -v -pattern gradient  -vsize $RES -pix_fmt $FMT -outname "$DIR/$TEST" ${BG}
 time ./patgen.exe -v -pattern test      -vsize $RES -pix_fmt $FMT -outname "$DIR/$TEST" -border -header -footer ${BG}
 
-wait 
+wait
 
 FMT=yuv444p
 time ./patgen.exe -v -pattern colorbar  -vsize $RES -pix_fmt $FMT -outname "$DIR/$TEST" ${BG}
@@ -51,6 +51,13 @@ time ./patgen.exe -v -pattern test      -vsize $RES -pix_fmt $FMT -outname "$DIR
 
 wait
 
+TEST=test53
+RES=4096x4096
+FMT=bgra
+time ./patgen.exe -v -pattern 16m_colors -vsize $RES -pix_fmt $FMT -outname "$DIR/$TEST" ${BG}
+FMT=yuv444p
+time ./patgen.exe -v -pattern 16m_colors -vsize $RES -pix_fmt $FMT -outname "$DIR/$TEST" ${BG}
+
 ../scripts/ff-csc.sh "$DIR"/test50-test-1280x720-bgra.rgb  yuv444p  yuv ${BG}
 ../scripts/ff-csc.sh "$DIR"/test52-colorbar-512x256-bgra.rgb  yuv444p  yuv ${BG}
 ../scripts/ff-csc.sh "$DIR"/test52-colorbar2-512x256-bgra.rgb  yuv444p  yuv ${BG}
@@ -59,17 +66,21 @@ wait
 ../scripts/ff-csc.sh "$DIR"/test52-test-512x256-bgra.rgb yuv444p  yuv ${BG}
 ../scripts/ff-csc.sh "$DIR"/test52-gradient-512x256-bgra.rgb yuv444p  yuv ${BG}
 
+../scripts/ff-csc.sh "$DIR"/test53-16m_colors-4096x4096-bgra.rgb yuv444p  yuv ${BG}
+
 wait
 
 
 # run these sequentially so logs are not mangled
 ../scripts/ff-psnr.sh  "$DIR"/test50-test-1280x720-yuv444p.yuv "$DIR"/test50-test-1280x720-yuv444p_bgra.yuv
 
-../scripts/ff-psnr.sh "$DIR"/test52-test-512x256-yuv444p.yuv "$DIR"/test52-test-512x256-yuv444p_bgra.yuv 
-../scripts/ff-psnr.sh "$DIR"/test52-colorbar-512x256-yuv444p.yuv "$DIR"/test52-colorbar-512x256-yuv444p_bgra.yuv 
+../scripts/ff-psnr.sh "$DIR"/test52-test-512x256-yuv444p.yuv "$DIR"/test52-test-512x256-yuv444p_bgra.yuv
+../scripts/ff-psnr.sh "$DIR"/test52-colorbar-512x256-yuv444p.yuv "$DIR"/test52-colorbar-512x256-yuv444p_bgra.yuv
 ../scripts/ff-psnr.sh "$DIR"/test52-colorbar2-512x256-yuv444p.yuv "$DIR"/test52-colorbar2-512x256-yuv444p_bgra.yuv
 ../scripts/ff-psnr.sh "$DIR"/test52-graybar-512x256-yuv444p.yuv "$DIR"/test52-graybar-512x256-yuv444p_bgra.yuv
 ../scripts/ff-psnr.sh "$DIR"/test52-hsv-512x256-yuv444p.yuv "$DIR"/test52-hsv-512x256-yuv444p_bgra.yuv
 ../scripts/ff-psnr.sh "$DIR"/test52-gradient-512x256-yuv444p.yuv "$DIR"/test52-gradient-512x256-yuv444p_bgra.yuv
+
+../scripts/ff-psnr.sh "$DIR"/test53-16m_colors-4096x4096-yuv444p.yuv "$DIR"/test53-16m_colors-4096x4096-yuv444p_bgra.yuv
 
 ../scripts/show-psnr-results.sh $DIR
